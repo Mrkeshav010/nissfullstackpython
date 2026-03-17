8d42ea86cf42df40f07a26fd099cd8fb8239d754
Keshab Kumar Nayak <mr.keshav010@gmail.com>
2026-03-17 09:05:06 +0530

    Looping in python

---
+++ NiisPython/Looping/demoLoop.py
@@ -0,0 +1,12 @@
+#Looping
+
+i=1
+print("A")
+while i<4:
+	print("B")
+	i=i+1
+	print("c")
+print("D")
+print(i)
+
+#Changed something to commit
---
+++ NiisPython/Looping/whileelse.py
@@ -0,0 +1,26 @@
+"""
+i=1
+while i<11:
+     print(i)
+     i=i+1  
+"""
+"""i=1
+while i<11:
+	print("hii")
+	if i<5:
+		print(i)
+	else:
+		break
+		i=i+1"""
+		    
+      
+i = 1
+while i <= 10:
+    if i == 4:
+        i += 1
+        continue
+    print(i, end="")
+    i += 1
+
+     
+     
